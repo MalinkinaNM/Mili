e72e3d8bfaaa95ab642373db31460439006bbc50
Malinkina Natalya <lesnatali1995@gmail.com>
2023-09-27 17:37:40 +0300

    Unitial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676; //стоимость билета
+        int miles = 20;     //кол-во рублей для одной бонусной мили
+        System.out.println("Колличество бонусных миль составляет:");
+        System.out.println(price / miles);
+    }
+}
